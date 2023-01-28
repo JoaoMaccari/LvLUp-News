8b2b812b5ea4a30bb67b55482e749635ea00964f
João Elias Maccari <joaomaccari@hotmail.com>
2023-01-28 11:50:21 -0300

    att

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");
-const app = express;
+const app = express();
 
-app.length("/", (req, res) =>{
-    res.send("bem vindo ao site")
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'))
+
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+app.get("/", (req, res) =>{
+    res.render('index');
 })
 
 
 app.listen(8080, ()=>{
-    console.log("Server rodando!")
+    console.log("Server rodando!");
 })
