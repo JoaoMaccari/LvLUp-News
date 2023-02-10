7d13d3b3c9169edaae72f36af08e1c8bddcf208a
João Elias Maccari <joaomaccari@hotmail.com>
2023-02-09 22:08:54 -0300

    att
    blog pag inicial

--- index.js
+++ index.js
@@ -29,8 +29,32 @@ app.use("/", categoriesController);
 app.use("/", articleController);
 
 app.get("/", (req, res) =>{
-    res.render('index');
+    Article.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then(articles => {
+        res.render('index', {articles: articles});
+    })
+    
 })
+
+app.get('/:slug', (req, res) =>{
+    var slug = req.params.slug;
+    Article.findOne({
+        where:{
+            slug: slug
+        }
+    }).then(article =>{
+        if(article != undefined){
+            res.render("article", {article:article});
+        }else{
+            res.redirect("/");
+        }
+    }).catch(err =>{
+        res.redirect('/')
+    })
+});
 
 app.listen(8080, ()=>{
     console.log("Server rodando!");
