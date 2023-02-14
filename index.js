c6f8dabcaba5c19addf78de4be47b1f6b0f9d67b
João Elias Maccari <joaomaccari@hotmail.com>
2023-02-14 15:24:21 -0300

    .

--- index.js
+++ index.js
@@ -34,8 +34,12 @@ app.get("/", (req, res) =>{
             ['id', 'DESC']
         ]
     }).then(articles => {
-        res.render('index', {articles: articles});
-    })
+
+        Category.findAll().then(categories =>{
+            res.render('index', {articles: articles, categories: categories});
+        });
+
+    });
     
 })
 
@@ -47,7 +51,10 @@ app.get('/:slug', (req, res) =>{
         }
     }).then(article =>{
         if(article != undefined){
-            res.render("article", {article:article});
+            Category.findAll().then(categories =>{
+                res.render('article', {articles: articles, categories: categories});
+            });
+
         }else{
             res.redirect("/");
         }
@@ -55,6 +62,27 @@ app.get('/:slug', (req, res) =>{
         res.redirect('/')
     })
 });
+
+app.get('/category/:slug', (req, res) =>{
+    var slug = req.params.slug;
+    Category.findOne({
+        where:{
+            slug: slug
+        },
+        include:[{model: Article}] 
+    }).then(category =>{
+        if(category != undefined){
+            Category.findAll().then(categories =>{
+                res.render('index', {articles: category.articles, categories: categories});
+            });
+        }else{
+            res.redirect('/')
+        }
+    }).catch(err =>{
+        res.redirect('/')
+    })
+})
+
 
 app.listen(8080, ()=>{
     console.log("Server rodando!");
