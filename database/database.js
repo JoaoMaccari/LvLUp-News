be4104d6a30f6a847ed0275617c6bd9b5db65565
João Elias Maccari <joaomaccari@hotmail.com>
2023-03-06 21:22:59 -0300

    deploy
    deploy vercel

--- database/database.js
+++ database/database.js
@@ -4,7 +4,8 @@ const Sequelize = require("sequelize");
 const Connection = new Sequelize('blog', 'root', 'meuservidor', {
     host: 'localhost',
     dialect: 'mysql',
-    timezone: '-03:00'
+    timezone: '-03:00',
+    dialectModule: require('mysql2'),
 });
 
 module.exports = Connection;
